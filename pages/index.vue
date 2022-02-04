b2e03f1a4859d0c9f464fd053f4aa1c5437d4b54
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-02-04 02:45:31 +0100

    nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <hero />
+  </div>
 </template>
 
 <script>
+import Hero from '~/components/Hero.vue'
 export default {
+  components: { Hero },
   name: 'IndexPage'
 }
 </script>
