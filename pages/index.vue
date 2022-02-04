b509945b5e02dd849ef7388f0fee609d21bf65f0
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-02-04 03:26:34 +0100

    nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,25 @@
 </template>
 
 <script>
+import axios from 'axios'
 import Hero from '~/components/Hero.vue'
 export default {
   components: { Hero },
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get('https://api.themoviedb.org/3/movie/550?api_key=0d7dff136b435e9f4b7c27f0b18ceabf');
+      const result = await data;
+      console.log(result.data);
+    }
+  }
 }
 </script>
