56057839aeb47fb26820a4806ab57e17315dda4c
tobidev <samueloluwatobiloba48@gmail.com>
2023-04-30 02:18:19 +0100

    new

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,7 @@
-export default {
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: 'movie-app',
-        htmlAttrs: {
-            lang: 'en'
-        },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
-            { name: 'format-detection', content: 'telephone=no' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
-    css: [
-        '@/assets/defaults.scss'
-    ],
-
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
-
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
-
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-    buildModules: [],
-
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [],
-
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {}
-}
+export default defineNuxtConfig({
+  routeRules: {
+    "/examples/*": { redirect: "/redirect-route" },
+    "/modify-headers-route": { headers: { "x-magic-of": "nuxt and vercel" } },
+    "/spa": { ssr: false },
+  },
+});
